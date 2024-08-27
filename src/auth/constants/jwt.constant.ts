1a2c7175df73fe52e02783ac8859f51610febcea
DARWIN UZCATEGUI <darwin.uzcategui1973@hotmail.com>
2024-08-27 15:33:18 -0400

    configuracion var ent jwt

--- src/auth/constants/jwt.constant.ts
+++ src/auth/constants/jwt.constant.ts
@@ -1,3 +1,6 @@
+console.log(process.env.JWT_TOKEN);
 export const jwtConstants = {
-  secret: 'no utilizar esta palabra en producción',
+  secret: process.env.JWT_TOKEN,
 };
+
+//secret: 'no utilizar esta palabra en producción',
